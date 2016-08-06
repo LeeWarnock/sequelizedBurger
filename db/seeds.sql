8aa2c4741946ac7fa13fe9ee5d27ecdf74c796ec
Lee Warnock <clwarnock@gmail.com>
2016-08-06 18:39:14 -0500

    pushes all files from old repo

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name) VALUES ('Runny Egg Burger');
+INSERT INTO burgers(burger_name) VALUES ('Questionably-Sourced Burger');
+INSERT INTO burgers(burger_name) VALUES ('Invisiburger');
+INSERT INTO burgers(burger_name) VALUES ('Inside-Out Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bourgeois Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Fried Burger');
+INSERT INTO burgers(burger_name) VALUES ('Booger Burger');
+INSERT INTO burgers(burger_name) VALUES ('BURGER! BURGER! BURGER');
